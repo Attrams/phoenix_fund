a3925b12fdd96ce7e8e6a4c0da14dbe24d182f7e
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-05-02 18:27:10 +0000

    add tailwind to watchers

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :phoenix_fund, PhoenixFundWeb.Endpoint,
   secret_key_base: "DGJjBnSQRt387i+nESP67PdKsKLjn0Xv0hAeSCVEfI7LT2SwT1GJ4O6/YUWQTmJC",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
